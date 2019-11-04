8b78b72942caa01d8585ffe2eca56cf6cb22b56c
zd <m15622189464@163.com>
2019-11-04 22:38:27 +0800

    增加swagger注释

--- web-api/src/main/java/com/ac/reserve/web/api/controller/LoginController.java
+++ web-api/src/main/java/com/ac/reserve/web/api/controller/LoginController.java
@@ -4,13 +4,15 @@ import com.ac.reserve.common.domain.BaseResponse;
 import com.ac.reserve.common.utils.ResponseBuilder;
 import com.ac.reserve.web.api.domain.User;
 import com.ac.reserve.web.api.service.WeChatService;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.HashMap;
 
 
 @RestController
@@ -20,8 +22,12 @@ public class LoginController {
     @Autowired
     private WeChatService weChatService;
 
+    @ApiOperation(value = "用户登录")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "code", value = "授权码", required = true, dataType = "code", paramType = "query"),
+    })
     @PostMapping("")
-    public BaseResponse login(@RequestParam(value = "code", required = false) String code) {
+    public BaseResponse login(@RequestParam(value = "code", required = true) String code) {
 
         User user = weChatService.login(code);
         return ResponseBuilder.buildSuccess(user);
