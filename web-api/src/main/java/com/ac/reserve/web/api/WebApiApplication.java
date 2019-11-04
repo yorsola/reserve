40dd8044aa6f4926a7d844dc286b68c382725087
zd <m15622189464@163.com>
2019-11-05 00:39:34 +0800

    增加表

--- web-api/src/main/java/com/ac/reserve/web/api/WebApiApplication.java
+++ web-api/src/main/java/com/ac/reserve/web/api/WebApiApplication.java
@@ -1,6 +1,5 @@
 package com.ac.reserve.web.api;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
@@ -13,7 +12,6 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableCaching
 @EnableSwagger2
 @EnableScheduling
-//@MapperScan("com.ac.reserve")
 @ComponentScan("com.ac.reserve")
 public class WebApiApplication {
 
--- web-api/src/main/java/com/ac/reserve/web/api/mapper/CampaignMapper.java
+++ web-api/src/main/java/com/ac/reserve/web/api/mapper/CampaignMapper.java
@@ -5,17 +5,8 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import org.apache.ibatis.annotations.Mapper;
 
 
-/**
- * <p>
-  *  Mapper 接口
- * </p>
- *
- * @author author
- * @since 2019-11-04
- */
 @Mapper
 public interface CampaignMapper extends BaseMapper<Campaign> {
-	
-	
-	
+
+
 }
