942a99184d2e8476cfc1afbe1ddcaef92b513741
zd <m15622189464@163.com>
2019-11-04 22:05:05 +0800

    增加登录拦截器

---
+++ common/src/main/java/com/ac/reserve/common/interceptor/LoginInterceptor.java
@@ -0,0 +1,36 @@
+package com.ac.reserve.common.interceptor;
+
+import com.ac.reserve.common.utils.RedisUtil;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class LoginInterceptor implements HandlerInterceptor {
+
+    @Autowired
+    private RedisUtil redisUtil;
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        String accessToken = request.getHeader("accessToken");
+        if (StringUtils.isNotBlank(accessToken) && redisUtil.hasKey(accessToken)) {
+            return true;
+        }
+        return false;
+
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+
+    }
+}
